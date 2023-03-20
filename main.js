c8f0eb0890eea80324449729aa1787135aa406cd
9oodam <godamcity98@gmail.com>
2023-03-20 14:25:42 +0900

    damin-3

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let orange = {
   name: "주황버섯",
   atk: 40,
   def: 0,
-  hp: 4000,
+  hp: 400,
   meso: 20,
 };
 let wolf = {
@@ -22,15 +22,16 @@ let wolf = {
 
 let user = {
   name: "유저",
-  atk: 100,
+  atk: 140,
   def: 10,
-  hp: 2000,
+  hp: 1200,
 };
 
 let monster = [];
 let monsterimg;
 
 let turncnt = 0;
+let level = 0;
 
 // 몬스터 정보 시작
 
@@ -44,7 +45,6 @@ function monsterinfo(name, atk, def, hp, meso) {
 
 function monsterspown() {
   let mon = Math.floor(Math.random() * 3 + 1);
-  console.log(mon);
   if (mon === 1) {
     monster = slime;
     monsterimg = document.querySelector(".monster").classList.add("slimeimg");
@@ -62,7 +62,12 @@ window.onload = monsterspown();
 
 // 몬스터 정보 끝
 
+
 // 공격 버튼 눌렀을때 시작
+
+let userdmg;
+let monsterdmg;
+
 function atk() {
   console.log(user);
   console.log(monster);
@@ -73,16 +78,22 @@ function atk() {
   if (usercri == 2 && user.atk - monster.def > 0) {
     console.log("유저크리티컬");
     console.log("몬스터 체력 : ", monster.hp);
-
-    monster.hp = monster.hp - (user.atk * 2 - monster.def);
+    
+    userdmg = user.atk * 2 - monster.def;
+    monster.hp = monster.hp - userdmg;
+    cri_num2(userdmg);
   } else if (user.atk <= monster.def) {
     console.log(
       "유저 대미지랑 몬스터 방어력이 같거나 유저대미지가 낮음 ",
       monster.hp
     );
+    miss();
   } else if (user.atk > monster.def) {
-    monster.hp = monster.hp - (user.atk - monster.def);
+    
+    userdmg = user.atk - monster.def;
+    monster.hp = monster.hp - userdmg;
     console.log("몬스터 체력 : ", monster.hp);
+    cri_num(userdmg);
   }
 
   if (monster.hp <= 0) {
@@ -94,24 +105,85 @@ function atk() {
       console.log("몬스터크리티컬");
       console.log("유저 체력 : ", user.hp);
 
-      user.hp = user.hp - (monster.atk * 2 - user.def);
+      monsterdmg = monster.atk * 2 - user.def;
+      user.hp = user.hp - monsterdmg;
+      cri_num2(monsterdmg);
     } else if (monster.atk <= user.def) {
       console.log(
         "몬스터 대미지랑 유저 방어력이 같거나 몬스터대미지가 낮음 ",
         monster.hp
       );
+      miss();
     } else if (monster.atk > user.def) {
-      user.hp = user.hp - (monster.atk - user.def);
+      monsterdmg = monster.atk - user.def;
+      user.hp = user.hp - monsterdmg;
       console.log("유저 체력 : ", user.hp);
+      cri_num(monsterdmg);
     }
 
     if (user.hp <= 0) {
       loserpop();
     }
-    console.log(user.hp);
-  }, 1000);
+  }, 3000);
 
   turncnt++;
+}
+
+function cri_num(dmg) {
+  let criticalNum = document.querySelector('.damage');
+  
+  let strArr = criticalNum.className.split(" ");
+
+  if(strArr.indexOf("damage_number") == -1) {
+      criticalNum.classList.add("damage_number");
+      criticalNum.classList.add("vibration");
+  }
+
+  document.querySelector('.number').innerHTML = dmg;
+  console.log("데미지: " + dmg);
+
+  setTimeout(() => {
+    document.querySelector('.number').innerHTML = '';
+    criticalNum.classList.remove("damage_number");
+    criticalNum.classList.removed("vibration");
+  }, 2000);
+}
+
+function cri_num2(dmg) {
+  let criticalNum = document.querySelector('.damage');
+  
+  let strArr = criticalNum.className.split(" ");
+
+  if(strArr.indexOf("damage_number_critical") == -1) {
+      criticalNum.classList.add("damage_number_critical");
+      criticalNum.classList.add("vibration");
+  }
+
+  document.querySelector('.critical').innerHTML = "CRITICAL\n" + dmg;
+
+  setTimeout(() => {
+    document.querySelector('.critical').innerHTML = '';
+    criticalNum.classList.remove("damage_number_critical");
+    criticalNum.classList.removed("vibration");
+  }, 2000);
+}
+
+function miss(dmg) {
+  let criticalNum = document.querySelector('.damage');
+  
+  let strArr = criticalNum.className.split(" ");
+
+  if(strArr.indexOf("miss_number") == -1) {
+      criticalNum.classList.add("miss_number");
+  }
+
+  document.querySelector('.number').innerHTML = "MISS";
+  console.log("데미지: " + dmg);
+
+  setTimeout(() => {
+    document.querySelector('.number').innerHTML = '';
+    criticalNum.classList.remove("miss_number");
+  }, 2000);
 }
 
 // 공격 버튼 눌렀을때 끝
@@ -134,10 +206,12 @@ function winnerpop() {
     turncnt + 1
   }</span>\n<span>보 상 : ${
     monster.meso
-  }</span>\n<div class="rebox">\n<div class="rebtn" onclick="">\n<span>다시하기</span></div></div>`;
+  }</span>\n<div class="rebox">\n<div class="rebtn" onclick="reload()">\n<span>다시하기</span></div></div>`;
   let test = document.querySelector(".winpopup");
   let tt = (test.innerHTML = winturn);
   // document.querySelector(".winpopup").innerHTML = winturn;
   test.push(tt);
+
+  level++;
 }
 // 유저가 이겼을때 보상 끝
