2fa9d5521e3123302f197ac0ec1208e37b6d3faf
9oodam <127164487+9oodam@users.noreply.github.com>
2023-03-21 02:44:04 +0900

    상점 수정

--- store.js
+++ store.js
@@ -41,3 +41,59 @@ document.querySelector(".tab03").onclick = function () {
   });
   container03.classList.add("is-active");
 };
+
+
+
+
+item = [];
+
+// 생성자 함수
+function iteminfo(name, atk, def, heel, price) {
+  this.name = name;
+  this.atk = atk;
+  this.def = def;
+  this.heel = heel;
+  this.price = price;
+}
+
+// 아이템
+let sword = new iteminfo("sword", 20, 0, 0, 1000);
+let shield = new iteminfo("shield", 0, 20, 0, 500);
+let red = new iteminfo("red", 0, 0, 100, 10);
+
+
+function buyItem(id) {
+  this.id = id;
+  console.log(id);
+  console.log("아이템 구매");
+
+  if(id == "sword") {
+    item = sword;
+    console.log(item);
+  } 
+  if(id == "shield") {
+    item = shield;
+    console.log(item);
+  }
+  if(id == "red") {
+    item = red;
+    console.log(item);
+  }
+  console.log("아이템 가격: " + item.price);
+
+  calculate();
+  moveItem();
+}
+
+function calculate() {
+  if(user.meso >= item.price) {
+    user.meso = user.meso - item.price;
+    console.log(user.meso);
+  } else if(user.meso <= item.price) {
+    alert("메소가 부족합니다.");
+  }
+}
+
+function moveItem() {
+
+}
