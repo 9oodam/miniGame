8efb83a6510d14871e08abaf65e92a5975e38bd4
9oodam <godamcity98@gmail.com>
2023-03-20 14:25:42 +0900

    damin-3

--- inventory.js
+++ inventory.js
@@ -11,3 +11,31 @@ function openInven() {
         popup.classList.add("is-active");
     }
 }
+
+// 무기 장착 했을 때
+function buff() {
+    let popup = document.querySelector('.weapon');
+    console.log(popup);
+    
+    let strArr = popup.className.split(" ");
+
+    if(strArr.indexOf("buff-on") != -1) {
+        popup.classList.remove("buff-on");
+    }else {
+        popup.classList.add("buff-on");
+    }
+}
+
+// 방어구 장착 했을 때
+function buff() {
+    let popup = document.querySelector('.ammor');
+    console.log(popup);
+    
+    let strArr = popup.className.split(" ");
+
+    if(strArr.indexOf("buff-on") != -1) {
+        popup.classList.remove("buff-on");
+    }else {
+        popup.classList.add("buff-on");
+    }
+}
